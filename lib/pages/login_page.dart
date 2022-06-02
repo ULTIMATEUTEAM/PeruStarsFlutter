6bc253147142a68bb9ae69520f47e848191c4488
DiegoAlonso0 <diegoalonsooliverab@gmail.com>
2022-06-02 09:57:51 -0500

    login page advanced

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:google_fonts/google_fonts.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 60),
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [Colors.red, Colors.red])),
+            child: SvgPicture.asset(
+              'assets/login_background.svg',
+              fit: BoxFit.cover,
+            ),
+            // Image.asset(
+            //   "assets/login_background.svg",
+            //   height: 200,
+            // ),
+          ),
+          Transform.translate(
+            offset: const Offset(0, -60),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  margin: const EdgeInsets.only(
+                      left: 20, right: 20, top: 260, bottom: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 35, vertical: 20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Iniciar Sesión',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Correo",
+                            prefixIcon: const Icon(Icons.person),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          style: GoogleFonts.montserrat(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Contraseña",
+                            prefixIcon: const Icon(Icons.lock),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          style: GoogleFonts.montserrat(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          obscureText: true,
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        ElevatedButton(
+                          onPressed: () => _login(context),
+                          child: Text(
+                            'Ingresar',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              onPrimary: Colors.white,
+                              fixedSize: const Size(160, 56),
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 17)),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('¿No tienes una cuenta?'),
+                            TextButton(
+                              child: const Text('Registrarse'),
+                              onPressed: () => _showRegister(context),
+                              style: TextButton.styleFrom(
+                                // Text color
+                                primary: Theme.of(context).primaryColor,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _login(BuildContext context) {
+    if (!_loading) {
+      setState(() {
+        _loading = true;
+      });
+    }
+  }
+
+  _showRegister(BuildContext context) {
+    Navigator.of(context).pushNamed('/register');
+  }
+}
